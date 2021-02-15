15f2343128a3417698ebe797d4e7cc836180f2ce
flandb <flandb.me@gmail.com>
2021-02-15 12:52:57 -0800

    change page title from default Next text to 'Auto Loans'

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home () {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Auto Loans</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
