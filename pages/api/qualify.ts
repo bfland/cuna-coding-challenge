a7f7780ccafc81a268e61611c107f18870e33bbb
flandb <flandb.me@gmail.com>
2021-02-15 20:54:41 -0800

    Add api route for determining qualification status

---
+++ pages/api/qualify.ts
@@ -0,0 +1,58 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import faker from 'faker'
+
+const MILLION = 1000000
+
+type FormData = {
+  make: string
+  model: string
+}
+
+type FormDataStrings = FormData & {
+  price: string
+  income: string
+  creditScore: string
+}
+
+type FormDataStringsNumbers = FormData & {
+  price: number
+  income: number
+  creditScore: number
+}
+
+const transform = (formData: FormDataStrings): FormDataStringsNumbers => {
+  return {
+    ...formData,
+    price: Number(formData.price),
+    income: Number(formData.income),
+    creditScore: Number(formData.creditScore)
+  }
+}
+
+const qualify = (formData: FormDataStringsNumbers): boolean => {
+  // If purchase price > 1/5th of income, return false (unqualified).
+  if (formData.price > formData.income / 5) return false
+
+  // If credit is less than 600, return false (unqualified).
+  if (formData.creditScore < 600) return false
+
+  return true
+}
+
+/**
+ * Notes:
+ * - I would normally implement some server-side validation if writing a complete app.
+ */
+const handler = (req: NextApiRequest, res: NextApiResponse): void => {
+  const data = transform(JSON.parse(req.body))
+
+  if (data.price > MILLION) {
+    res.status(400).json({ message: 'Price must be less than 1,000,000.' })
+    return
+  }
+
+  const qualified = qualify(data)
+  res.status(200).json({ qualified })
+}
+
+export default handler
