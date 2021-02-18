f86f877bd306c6000c0eac0a546c9433c7152f76
flandb <flandb.me@gmail.com>
2021-02-17 20:00:24 -0800

    use fakerjs lib to generate phone number.

--- pages/disqualified.tsx
+++ pages/disqualified.tsx
@@ -1,11 +1,12 @@
+import faker from 'faker'
+
 const Disqualified = ({ message }) => {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Sorry, you do not qualify for a loan at this time.</h1>
       <p>The reasons for this decision include: {message}</p>
       <p>
-        If you have questions, customer service can be reached by calling: (123)
-        456-7890
+        If you have questions, customer service can be reached by calling: {faker.phone.phoneNumberFormat()}
       </p>
     </div>
   )
