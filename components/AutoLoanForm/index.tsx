dd0a2e60bb98ff77ce6d4dc60bcf902c80d0f4b9
flandb <flandb.me@gmail.com>
2021-02-17 20:01:45 -0800

    Convert AutoLoanForm component to use composition with Form and FormField components.

--- components/AutoLoanForm/index.tsx
+++ components/AutoLoanForm/index.tsx
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import { withRouter, NextRouter } from 'next/router'
+import { Form, FormField } from '../Form'
 import styles from './styles.module.css'
 
 type State = {
@@ -67,73 +68,54 @@ class AutoLoanForm extends Component<FormProps> {
     return (
       <div className={styles.autoLoanForm}>
         <h1>Auto Loan Application</h1>
-        <form className={styles.form} onSubmit={this.onFormSubmit}>
-          <div className={styles.field}>
-            <label>Auto Purchase Price</label>
-            <div>
-              ${' '}
-              <input
-                type='number'
-                min={1}
-                value={this.state.price}
-                onChange={e => this.setState({ price: e.target.value })}
-                required
-              />
-            </div>
-          </div>
+        <Form error={this.state.error} onSubmit={this.onFormSubmit}>
+          <FormField
+            label='Auto Purchase Price'
+            type='number'
+            value={this.state.price}
+            prefix='$'
+            onChange={e => this.setState({ price: e.target.value })}
+            inputProps={{
+              min: 1
+            }}
+          />
 
-          <div className={styles.field}>
-            <label>Auto Make</label>
-            <input
-              type='text'
-              value={this.state.make}
-              onChange={e => this.setState({ make: e.target.value })}
-              required
-            />
-          </div>
+          <FormField
+            label='Auto Make'
+            type='text'
+            value={this.state.make}
+            onChange={e => this.setState({ make: e.target.value })}
+          />
 
-          <div className={styles.field}>
-            <label>Auto Model</label>
-            <input
-              type='text'
-              value={this.state.model}
-              onChange={e => this.setState({ model: e.target.value })}
-              required
-            />
-          </div>
+          <FormField
+            label='Auto Model'
+            type='text'
+            value={this.state.model}
+            onChange={e => this.setState({ model: e.target.value })}
+          />
 
-          <div className={styles.field}>
-            <label>Estimated Yearly Income</label>
-            <div>
-              ${' '}
-              <input
-                type='number'
-                min={0}
-                value={this.state.income}
-                onChange={e => this.setState({ income: e.target.value })}
-                required
-              />
-            </div>
-          </div>
+          <FormField
+            label='Estimated Yearly Income'
+            type='number'
+            value={this.state.income}
+            prefix='$'
+            onChange={e => this.setState({ income: e.target.value })}
+            inputProps={{
+              min: 0
+            }}
+          />
 
-          <div className={styles.field}>
-            <label>Estimated Credit Score</label>
-            <input
-              type='number'
-              min={300}
-              max={850}
-              value={this.state.creditScore}
-              onChange={e => this.setState({ creditScore: e.target.value })}
-              required
-            />
-          </div>
-
-          <button type='submit' className={styles.button}>
-            Apply
-          </button>
-        </form>
-
-        <div className={styles.error}>{this.state.error}</div>
+          <FormField
+            label='Estimated Credit Score'
+            type='number'
+            value={this.state.creditScore}
+            onChange={e => this.setState({ creditScore: e.target.value })}
+            inputProps={{
+              min: 300,
+              max: 850
+            }}
+          />
+        </Form>
       </div>
     )
   }
