3977f07172c09e42a0be0ca7a5392a79391292d2
flandb <flandb.me@gmail.com>
2021-02-17 18:32:27 -0800

    Add 'message' state and pass to page components.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
+import { useState } from 'react'
 import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }: AppProps) {
+  // I know, I know! I took the liberty of a shortcut here - this is not a
+  // solution I would ever use in a real-world app to pass state to components,
+  // as now all pages receive the 'message' and 'setMessage' props... this
+  // could be improved/remedied with the use of Redux action/reducer flow
+  // at a lower level.
+  const [disqualifiedMsg, setDisqualifiedMsg] = useState('')
+
+  return (
+    <Component
+      {...pageProps}
+      message={disqualifiedMsg}
+      setMessage={setDisqualifiedMsg}
+    />
+  )
 }
 
 export default MyApp
