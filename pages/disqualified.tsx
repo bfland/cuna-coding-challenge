8ee5e5858fe9fe79c601a40567aadcb02a23e5d7
flandb <flandb.me@gmail.com>
2021-02-17 18:50:09 -0800

    Populate 'disqualified' page with denial message, and fake customer service info.

--- pages/disqualified.tsx
+++ pages/disqualified.tsx
@@ -1,5 +1,14 @@
 const Disqualified = ({ message }) => {
-    return <div>Disqualified</div>
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Sorry, you do not qualify for a loan at this time.</h1>
+      <p>The reasons for this decision include: {message}</p>
+      <p>
+        If you have questions, customer service can be reached by calling: (123)
+        456-7890
+      </p>
+    </div>
+  )
 }
 
 export default Disqualified
