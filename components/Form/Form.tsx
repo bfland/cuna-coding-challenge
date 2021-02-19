4f48891182feec92eb77e689d31384d2fb60edfd
flandb <flandb.me@gmail.com>
2021-02-18 18:58:56 -0800

    Move repeated code to form (form title and container styling).

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,19 +1,25 @@
 import styles from './Form.styles.module.css'
 
 type FormProps = {
-    children: any
-    onSubmit: (event: any) => Promise<void>
-    error?: string
+  children: any
+  onSubmit: (event: any) => Promise<void>
+  error?: string
+  title?: string
 }
 
-const Form = ({ children, error, onSubmit }: FormProps) => {
-    return (
-        <form className={styles.form} onSubmit={onSubmit}>
-            {children}
-            <button type='submit' className={styles.button}>Apply</button>
-            {error && <div className={styles.error}>{error}</div>}
-        </form>
-    )
+const Form = ({ children, error, onSubmit, title }: FormProps) => {
+  return (
+    <div className={styles.formContainer}>
+      <h1>{title}</h1>
+      <form className={styles.form} onSubmit={onSubmit}>
+        {children}
+        <button type='submit' className={styles.button}>
+          Apply
+        </button>
+        {error && <div className={styles.error}>{error}</div>}
+      </form>
+    </div>
+  )
 }
 
 export default Form
